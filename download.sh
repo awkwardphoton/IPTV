9d64dc11afba8ea232b1ba5c6c03ef0d6733334d
Ethan Walker <awkwardphoton@gmail.com>
2018-12-09 23:35:54 +0000

    Update and rename dw.sh to download.sh

---
+++ download.sh
@@ -0,0 +1,7 @@
+get-iplayer --get "Doctor Who" --output ~/Videos/TV/Doctor_Who/ --fps25 --file-prefix="S<00seriesnum>E<00episodenum>-<episodeshort>" --force
+
+get-iplayer --get "Dynasties" --output ~/Videos/TV/Documentaries/Dynasties/  --file-prefix="S<00seriesnum>E<00episodenum>-<episodeshort>" 
+
+get-iplayer --get "QI" --output ~/Videos/TV/Documentaries/QI/  --file-prefix="S<00seriesnum>E<00episodenum>-<episodeshort>"
+
+get-iplayer --get Louis Theroux --output ~/Videos/TV/Documentaries/Theroux/  --file-prefix="S<00seriesnum>E<00episodenum>-<episodeshort>"
