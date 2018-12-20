254f4fb8a2a4e356b68d475d77c27b535dc5f8ba
Ethan Walker <awkwardphoton@gmail.com>
2018-12-20 00:26:47 +0000

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,5 +1,4 @@
-get-iplayer --get "Doctor Who" --output ~/Videos/TV/Doctor_Who/ --fps25 --file-prefix="S<00seriesnum>E<00episodenum>-<episodeshort>" --force
-
+get-iplayer --get "Doctor Who" --output ~/Videos/TV/Doctor_Who/ --fps25 --file-prefix="S<00seriesnum>E<00episodenum>-<episodeshort>" 
 get-iplayer --get "Dynasties" --output ~/Videos/TV/Documentaries/Dynasties/  --file-prefix="S<00seriesnum>E<00episodenum>-<episodeshort>" 
 
 get-iplayer --get "QI" --output ~/Videos/TV/Documentaries/QI/  --file-prefix="S<00seriesnum>E<00episodenum>-<episodeshort>"
