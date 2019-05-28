6eb4e7415faa03d979c262c7eff17d27ddfc8368
Ethan Walker <awkwardphoton@gmail.com>
2019-05-28 22:34:06 +0100

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,1 +1,1 @@
-get-iplayer --get "The Planets" --output '/home/master/Videos/TV/The Planets' --file-prefix="S<00seriesnum>E<00episodenum>-<episodeshort>" 
+get-iplayer --get "The Planets" --output '/media/master/c9117ef3-0223-4042-ad18-776a325a3/TV/The Planets' --file-prefix="S<00seriesnum>E<00episodenum>-<episodeshort>" 
